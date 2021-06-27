2feb87e5729c0b5c5f8660f9106507723acaaf5c
Yana Strunina <yana.strunina.9831.g@gmail.com>
2021-06-27 19:56:59 +0500

    refactoring options validation

--- src/slider/MVC/Model.ts
+++ src/slider/MVC/Model.ts
@@ -15,25 +15,25 @@ import {
 } from "./actionTypes";
 import { EventEmitter } from "./EventEmmiter";
 
-interface ISliderEvent{
-  action:string,
-  payload:number|boolean|string|object
+interface ISliderEvent {
+  action: string;
+  payload: number | boolean | string | object;
 }
-interface IOptions  {
-  firstValue? : number,
-  secondValue?:number,
-  step?: number,
-  range?:boolean,
-  hasScale?:boolean,
-  hasTips?:boolean,
-  vertical?:boolean,
-  min?:number,
-  max?:number,
-  maxScaleNumbersCount?:number,
-  scaleValues?:number[],
+interface IOptions {
+  firstValue?: number;
+  secondValue?: number;
+  step?: number;
+  range?: boolean;
+  hasScale?: boolean;
+  hasTips?: boolean;
+  vertical?: boolean;
+  min?: number;
+  max?: number;
+  maxScaleNumbersCount?: number;
+  scaleValues?: number[];
 }
 
-const defaultOptions:IOptions = {
+const defaultOptions: IOptions = {
   firstValue: 0,
   secondValue: 5,
   step: 1,
@@ -47,188 +47,108 @@ const defaultOptions:IOptions = {
 };
 
 class Model extends EventEmitter {
-
-  private options:IOptions
+  private options: IOptions;
 
   constructor(options: IOptions) {
     super();
     this.options = options;
   }
- private validateOptions(options: IOptions):IOptions {
-    if (typeof options !== "object") {
-      return defaultOptions;
-    }
-    let validOptions :IOptions= { ...options };
-    validOptions.range = !!validOptions.range;
-    validOptions.hasScale = !!validOptions.hasScale;
-    validOptions.hasTips = !!validOptions.hasTips;
-    validOptions.vertical = !!validOptions.vertical;
-    if (
-      typeof validOptions.min !== "number" &&
-      typeof validOptions.max !== "number"
-    ) {
-      validOptions.min = 0;
-      validOptions.max =
-        typeof validOptions.step === "number"
-          ? validOptions.min + validOptions.step * 10
-          : validOptions.min + 10;
-    } else if (
-      typeof validOptions.min !== "number" &&
-      typeof validOptions.max === "number"
-    ) {
-      validOptions.min =
-        typeof validOptions.step === "number"
-          ? validOptions.max - validOptions.step * 10
-          : validOptions.max - 10;
-    } else if (
-      typeof validOptions.min === "number" &&
-      typeof validOptions.max !== "number"
-    ) {
-      validOptions.max =
-        typeof validOptions.step === "number"
-          ? validOptions.min + validOptions.step * 10
-          : validOptions.min + 10;
-    }
-    if (validOptions.min > validOptions.max) {
-      let val = validOptions.min;
-      validOptions.min = validOptions.max;
-      validOptions.max = val;
-    }
 
-    if (typeof validOptions.step !== "number") {
-      validOptions.step = (validOptions.max - validOptions.min) / 10;
-    }
-
-    let stepValues:number[] = [];
-    let valueInRange:number = validOptions.min;
-    while (valueInRange < validOptions.max) {
-      stepValues.push(valueInRange);
-      valueInRange = valueInRange + validOptions.step;
-    }
-    stepValues.push(validOptions.max);
-
-    validOptions.maxScaleNumbersCount =
-      typeof validOptions.maxScaleNumbersCount !== "number"
-        ? 10
-        : validOptions.maxScaleNumbersCount < 2
-        ? 2
-        : Math.ceil(validOptions.maxScaleNumbersCount);
-
-    let indexDiff:number = Math.ceil(
-      stepValues.length / (validOptions.maxScaleNumbersCount - 1)
-    );
-    let scaleValues:number[] = [];
-    let index:number = 0;
-    while (index < stepValues.length - 1) {
-      scaleValues.push(stepValues[index]);
-      index = index + indexDiff;
-    }
-    scaleValues.push(stepValues[stepValues.length - 1]);
-
-    validOptions.scaleValues = scaleValues;
-
-    validOptions.firstValue =
-      typeof validOptions.firstValue !== "number"
-        ? validOptions.min
-        : this.findClosestValue(validOptions.firstValue, stepValues);
-    validOptions.secondValue =
-      typeof validOptions.secondValue !== "number"
-        ? validOptions.max
-        : this.findClosestValue(validOptions.secondValue, stepValues);
-
-    if (validOptions.firstValue > validOptions.secondValue) {
-      let val = validOptions.firstValue;
-      validOptions.firstValue = validOptions.secondValue;
-      validOptions.secondValue = val;
-    }
-    if (!validOptions.range) {
-      validOptions.firstValue = validOptions.min;
-    }
-    console.log(validOptions);
-    return validOptions;
-  }
-
- private findClosestValue(value:number, array:number[]):number {
-    let diffArray:number[] = [];
-    array.forEach(item=>diffArray.push(Math.abs(item - value)))
-    let min:number = Math.min(...diffArray);
-    let minIndex:number = diffArray.findIndex((item) => item === min);
-    return array[minIndex];
-  }
-
- public firstLoadOptions():void {
+  public firstLoadOptions(): void {
     this.options = this.validateOptions(this.options);
     this.emit(FIRST_LOAD_OPTIONS, { ...this.options });
   }
 
- public setMinValue(min:number):void {
+  public setMinValue(min: number): void {
     this.eventHandler({ action: MIN_VALUE_CHANGED, payload: min });
   }
-  public  getMinValue():number {
+
+  public getMinValue(): number {
     return this.options.min;
   }
-  public  setMaxValue(max:number):void {
+
+  public setMaxValue(max: number): void {
     this.eventHandler({ action: MAX_VALUE_CHANGED, payload: max });
   }
-  public  getMaxValue():number {
+
+  public getMaxValue(): number {
     return this.options.max;
   }
-  public  setFirstValue(value:number):void {
+
+  public setFirstValue(value: number): void {
     this.eventHandler({ action: FIRST_VALUE_CHANGED, payload: value });
   }
-  public  getFirstValue():number {
+
+  public getFirstValue(): number {
     return this.options.firstValue;
   }
-  public  setSecondValue(value:number):void {
+
+  public setSecondValue(value: number): void {
     this.eventHandler({ action: SECOND_VALUE_CHANGED, payload: value });
   }
-  public  getSecondValue():number {
+
+  public getSecondValue(): number {
     return this.options.secondValue;
   }
-  public  setHasTips(hasTips:boolean):void {
+
+  public setHasTips(hasTips: boolean): void {
     this.eventHandler({ action: HAS_TIPS_CHANGED, payload: hasTips });
   }
-  public  getHasTips():boolean {
+
+  public getHasTips(): boolean {
     return this.options.hasTips;
   }
-  public  setRange(range:boolean):void {
+
+  public setRange(range: boolean): void {
     this.eventHandler({ action: RANGE_CHANGED, payload: range });
   }
-  public  getRange():boolean {
+
+  public getRange(): boolean {
     return this.options.range;
   }
-  public  setHasScale(hasScale:boolean):void {
+
+  public setHasScale(hasScale: boolean): void {
     this.eventHandler({ action: HAS_SCALE_CHANGED, payload: hasScale });
   }
-  public  getHasScale():boolean {
+
+  public getHasScale(): boolean {
     return this.options.hasScale;
   }
-  public  setVertical(vertical:boolean):void {
+
+  public setVertical(vertical: boolean): void {
     this.eventHandler({ action: VERTICAL_CHANGED, payload: vertical });
   }
-  public  getVertical():boolean {
+
+  public getVertical(): boolean {
     return this.options.vertical;
   }
-  public  setStep(step:number):void {
+
+  public setStep(step: number): void {
     this.eventHandler({ action: STEP_CHANGED, payload: step });
   }
-  public   getStep():number {
+
+  public getStep(): number {
     return this.options.step;
   }
-  public setMaxScaleNumbersCount(count:number):void {
+
+  public setMaxScaleNumbersCount(count: number): void {
     this.eventHandler({
       action: MAX_SCALE_NUMBERS_COUNT_CHANGED,
       payload: count,
     });
   }
-  public  getMaxScaleNumbersCount():number {
+
+  public getMaxScaleNumbersCount(): number {
     return this.options.maxScaleNumbersCount;
   }
- public eventHandler(event:ISliderEvent):void {
+
+  public eventHandler(event: ISliderEvent): void {
     this.reducer(event.action, event.payload);
   }
- private reducer(action: string, value:number|boolean|string|object) {
-    let options :IOptions = { ...this.options };
+
+  private reducer(action: string, value: number | boolean | string | object) {
+    let options: IOptions = { ...this.options };
+
     switch (action) {
       case FIRST_VALUE_CHANGED:
         options.firstValue = Number(value);
@@ -263,10 +183,10 @@ class Model extends EventEmitter {
         options.range = Boolean(value);
         break;
       case HAS_SCALE_CHANGED:
-        options.hasScale =Boolean(value);
+        options.hasScale = Boolean(value);
         break;
       case VERTICAL_CHANGED:
-        options.vertical =Boolean(value);
+        options.vertical = Boolean(value);
         break;
       case STEP_CHANGED:
         options.step = Number(value);
@@ -275,8 +195,159 @@ class Model extends EventEmitter {
         options.maxScaleNumbersCount = Number(value);
         break;
     }
+
     this.options = this.validateOptions(options);
     this.emit(MODEL_CHANGED, { ...this.options });
+  }
+
+  private validateOptions(options: IOptions): IOptions {
+    if (typeof options !== "object") {
+      return defaultOptions;
+    }
+
+    let validOptions: IOptions = { ...options };
+
+    validOptions.range = !!validOptions.range;
+    validOptions.hasScale = !!validOptions.hasScale;
+    validOptions.hasTips = !!validOptions.hasTips;
+    validOptions.vertical = !!validOptions.vertical;
+
+    this.getValidMinMaxValues(validOptions);
+
+    validOptions.step = this.getValidStep(
+      validOptions.step,
+      validOptions.min,
+      validOptions.max
+    );
+
+    validOptions.maxScaleNumbersCount = this.getValidMaxScaleNumbersCount(
+      validOptions.maxScaleNumbersCount
+    );
+
+    validOptions.scaleValues = this.getScaleValues(
+      validOptions.min,
+      validOptions.max,
+      validOptions.step,
+      validOptions.maxScaleNumbersCount
+    );
+
+    validOptions.firstValue =
+      typeof validOptions.firstValue !== "number" || !validOptions.range
+        ? validOptions.min
+        : this.findClosestScaleValue(
+            validOptions.min,
+            validOptions.max,
+            validOptions.step,
+            validOptions.firstValue
+          );
+
+    validOptions.secondValue =
+      typeof validOptions.secondValue !== "number"
+        ? validOptions.max
+        : this.findClosestScaleValue(
+            validOptions.min,
+            validOptions.max,
+            validOptions.step,
+            validOptions.secondValue
+          );
+
+    if (validOptions.firstValue > validOptions.secondValue) {
+      const val = validOptions.firstValue;
+      validOptions.firstValue = validOptions.secondValue;
+      validOptions.secondValue = val;
+    }
+
+    return validOptions;
+  }
+
+  private getValidMinMaxValues(options: IOptions): void {
+    const isBothNotNumbers =
+      typeof options.min !== "number" && typeof options.max !== "number";
+    const isOnlyMaxNumber =
+      typeof options.min !== "number" && typeof options.max === "number";
+    const isOnlyMinNumber =
+      typeof options.min === "number" && typeof options.max !== "number";
+    const isStepValidNumber =
+      typeof options.step === "number" && options.step > 0;
+
+    if (isBothNotNumbers) {
+      options.min = 0;
+      options.max = isStepValidNumber
+        ? options.min + options.step * 10
+        : options.min + 10;
+    } else if (isOnlyMaxNumber) {
+      options.min = isStepValidNumber
+        ? options.max - options.step * 10
+        : options.max - 10;
+    } else if (isOnlyMinNumber) {
+      options.max = isStepValidNumber
+        ? options.min + options.step * 10
+        : options.min + 10;
+    }
+    if (options.min > options.max) {
+      const val = options.min;
+      options.min = options.max;
+      options.max = val;
+    }
+  }
+
+  private findClosestScaleValue(
+    min: number,
+    max: number,
+    step: number,
+    value: number
+  ): number {
+    if (value - min < 0) return min;
+    if (max - value < (max - Math.floor(max / step) * step) / 2) {
+      return max;
+    }
+
+    let valDiff = value - min;
+    let stepsCount = Math.round(valDiff / step);
+    let stepValue = min + stepsCount * step;
+
+    if (stepValue > max) {
+      return max;
+    } else {
+      return stepValue;
+    }
+  }
+
+  private getScaleValues(
+    min: number,
+    max: number,
+    step: number,
+    maxScaleNumbersCount: number
+  ): Array<number> {
+    const stepValuesCount = Math.ceil((max - min) / step) + 1;
+    const indexDiff = Math.ceil(stepValuesCount / maxScaleNumbersCount);
+
+    let scaleValues: number[] = [];
+    for (let i = 0; i < stepValuesCount - 1; i = i + indexDiff) {
+      scaleValues.push(min + i * step);
+    }
+    scaleValues.push(max);
+
+    return scaleValues;
+  }
+
+  private getValidMaxScaleNumbersCount(maxScaleNumbersCount: number): number {
+    const validMaxScaleNumbersCount =
+      typeof maxScaleNumbersCount !== "number"
+        ? 10
+        : maxScaleNumbersCount < 2
+        ? 2
+        : Math.ceil(maxScaleNumbersCount);
+
+    return validMaxScaleNumbersCount;
+  }
+
+  private getValidStep(step: number, min: number, max: number): number {
+    if (typeof step !== "number" || step <= 0) {
+      return (max - min) / 10;
+    }
+
+    return step;
   }
 }
 export { Model, IOptions, ISliderEvent };
